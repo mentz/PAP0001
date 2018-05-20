9170a5974c6c854c4c9bb8d62d2e2db46c071976
andreepdias <andre.dias@msn.com>
2018-05-20 18:37:22 -0300

    Client enviando dados, servidor armazenando dados mas ainda não enviando para a aplicação c++

--- Servidor/mysocketserver.py
+++ Servidor/mysocketserver.py
@@ -1,19 +1,26 @@
 import socketserver
 
+# armazena id do avião, num_parada, carga, latitute, longitude
+paradas = []
+
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print("{} HAS JUST SENT:\n".format(self.client_address[0]))
+
+        print("{} ACABOU DE ENVIAR:\n".format(self.client_address[0]))
         data = str(self.data)
-        data = data.split()
+        data = data.split(";")
         print(data)
-        # if(data[0] == "b'SENT:"):
-        #     print("xxx")
-        self.request.sendall(bytes("COORDENADAS E CARGA RECEBIDAS", "utf-8"))
+
+        if(data[1] == "0"):
+            paradas.append([data[2], data[3], data[4], data[5], data[6]])
+
+
+        self.request.sendall(bytes("DADOS RECEBIDOS", "utf-8"))
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9990
+    HOST, PORT = "localhost", 9999
 
     # Create the server, binding to localhost on port 9999
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
