8f20eccc6d42ca930214dcc59af5d88ba3a27587
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2018-05-20 21:47:18 -0300

    Mudado algumas coisas

--- Servidor/mysocketserver.py
+++ Servidor/mysocketserver.py
@@ -1,3 +1,6 @@
+#!/usr/sbin/python3
+#! coding=utf-8
+
 import socketserver
 import sys
 
@@ -6,36 +9,37 @@ paradas = []
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
+	def handle(self):
+		self.data = self.request.recv(1024).strip()
+		data = str(self.data)
+		data = data.split(";")
 
-        print("{} ACABOU DE ENVIAR:\n".format(self.client_address[0]))
-        data = str(self.data)
-        data = data.split(";")
-        print(data)
+		print("Redebido de %s: %s" %(self.client_address[0], data))
 
-        if(data[1] == "0"):  #Guarda dados
-            paradas.append([data[2], data[3], data[4], data[5], data[6]])
-            self.request.sendall(bytes("DADOS RECEBIDOS", "utf-8"))
+		if(data[1] == "0"):  #Guarda dados
+			paradas.append([data[2], data[3], data[4], data[5], data[6]])
+			self.request.sendall(bytes("DADOS RECEBIDOS", "utf-8"))
 
-        if(data[1] == "1"): #Envia dados
-            #Considerando que o reques seja no formato 'x;1;n;x', onde n é o índice do próximo registro que o cliente_framebuffer espera
-            i = 0
-            for stop in paradas:
-                if(i > data[2]):
-                    envio += str("x;" + stop[0] + ";" + stop[1] + ";" + stop[2] + ";" + stop[3] + ";" + stop[4] + ";x")
-                i = i + 1 
-                
-            self.request.sendall(bytes(envio, "utf-8"))
+		if(data[1] == "1"): #Envia dados
+			# Considerando que o request seja no formato 'x;1;n;x', onde n é o
+			# índice do próximo registro que o cliente_framebuffer espera
+			i = 0
+			for stop in paradas:
+				if(i > data[2]):
+					envio += str("x;" + stop[0] + ";" + stop[1] + ";" + stop[2] + ";" + stop[3] + ";" + stop[4] + ";x")
+				i = i + 1 
+				
+			self.request.sendall(bytes(envio, "utf-8"))
 
 if __name__ == "__main__":
-    PORT = 9990
-    print("Digite o endereco do servidor: ")
-    HOST = input()
+	PORT = 16368
+	print("Digite o endereco do servidor: ")
+	HOST = input()
 
-    # Create the server, binding to localhost on port 9990
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+	print("Iniciando servidor")
+	# Create the server, binding to localhost on port 9990
+	server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+	# Activate the server; this will keep running until you
+	# interrupt the program with Ctrl-C
+	server.serve_forever()
