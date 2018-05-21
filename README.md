312172f793d72c02eb0c992e564985db8af1f6c9
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2018-05-21 09:40:03 -0300

    Reorganizado, atualizado README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PAP0001
-Frame BUFF
+Repositório de tarefas e trabalhos das aulas de Paradigmas de Programação
+
+#Trabalho com Linux framebuffer/ioctl()
+Os três componentes:
+* server.py
+* client.py
+* fbclient
+devem ser abertos, nessa ordem, e o client.py deve ser alimentado de ao menos um registro antes de abrir o fbclient.
---
+++ cliente.py
@@ -0,0 +1,47 @@
+#!/usr/sbin/python3
+#! coding=utf-8
+
+import socket
+import sys
+import time
+from random import randrange, uniform
+
+PORT = 9998
+HOST = ""
+
+print("Digite o IP do servidor")
+HOST = input();
+
+while True:
+	try:
+		print("Digite o numero do aviao: ")
+		num_aviao = int(input())
+		print("Digite o numero da parada: ")
+		num_parada = int(input())
+		while True:
+			print("Digite a latitude: ")
+			lat = float(input())
+			if(not (lat > 90 or lat < -90)):
+				break
+			print("Latitude deve estar entre -90 e 90, tente novamente.")
+
+		while True:
+			print("Digite a longitude: ")
+			long = float(input())
+			if(not (long > 180 or long <= -180)):
+				break
+			print("Longitude deve estar entre -180 e 180, tente novamente.")
+
+		print("Digite qual alteração na carga (positivo para carga e negativo para descarga): ")
+		carga = float(input())
+		data = "x;0;" + str(num_aviao) + ";" + str(num_parada) + ";" + str(lat) + ";" + str(long) + ";" + str(carga) + ";x"
+
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect((HOST, PORT))
+		sock.sendall(bytes(data, "utf-8"))
+		received = str(sock.recv(1024), "utf-8")
+	finally:
+		sock.close()
+	print("Sent:	 %s" % data)
+	print("Received: %s" % received)
+	time.sleep(1)
